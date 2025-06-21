b3858d2dbd3c1e894e17338c3ba66ec8e08214ea
Pedp4 WPBX4125BLF102401423 <wpbx4125blf102401423@pedp4.onmicrosoft.com>
2025-06-21 23:56:44 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Registration from "./pages/Registration";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route element={<Home />} path="/" exact />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Profile />} path="/me" />
+        <Route element={<Registration />} path="/register" />
+        <Route element={<NotFound />} path="*" />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
